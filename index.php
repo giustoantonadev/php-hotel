769fcb921b7238b5a6272f7c8bc9d7c68f4be4ea
giustoantonadev <giustoantonadev@gmail.com>
2026-06-08 17:27:49 +0200

    Add Bootstrap and upgraded hotel table with Bootstrap styling

--- index.php
+++ index.php
@@ -1,4 +1,18 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hotels</title>
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+
+<body class="p-4">
+
+    <?php
 
     $hotels = [
 
@@ -40,12 +54,22 @@
 
     ];
 
+    echo "<table class='table table-striped table-bordered'>";
+    echo "<tr><th>Nome</th>  <th>Descrizione</th> <th>Parcheggio</th> <th>Voto</th> <th>Distanza dal centro (km)</th></tr>";
+
     foreach ($hotels as $hotel) {
-        echo "Nome: " . $hotel["name"] . "<br>";
-        echo "Descrizione: " . $hotel["description"] . "<br>";
-        echo "Parcheggio: " . ($hotel["parking"] ? "Sì" : "No") . "<br>";
-        echo "Voto: " . $hotel["vote"] . "<br>";
-        echo "Distanza dal centro: " . $hotel["distance_to_center"] . " km<br><br>";
+        echo "<tr>";
+        echo "<td>" . $hotel["name"] . "</td>";
+        echo "<td>" . $hotel["description"] . "</td>";
+        echo "<td>" . ($hotel["parking"] ? "Sì" : "No") . "</td>";
+        echo "<td>" . $hotel["vote"] . "</td>";
+        echo "<td>" . $hotel["distance_to_center"] . "</td>";
+        echo "</tr>";
     }
 
-?>
+    echo "</table>";
+
+    ?>
+
+</body>
+</html>
